22a0d0f01915ce14a7188d105824ed72b909216e
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-03-31 17:41:58 +0200

    terraform a test rds

---
+++ terraform/rds/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  profile = "${var.profile}"
+  region = "${var.region}"
+  version = "~> 2.55"
+}
+# data "aws_vpc" "vpc" {
+#   id = "${var.vpc_id}"
+# }
+
+# data "aws_availability_zones" "available" {
+#   state = "available"
+# }
