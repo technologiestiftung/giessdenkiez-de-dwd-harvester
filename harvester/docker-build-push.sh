d7f8d1fe2ca676a64f959e1d9b2c4a6b041360c5
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-03-31 15:51:09 +0200

    test

--- harvester/docker-build-push.sh
+++ harvester/docker-build-push.sh
@@ -8,17 +8,20 @@ IFS=$'\n\t'
 GITHUB_REPOSITORY="technologiestiftung/dwd-radolan-tree-harvester"
 TAG="test"
 DOCKERFILE="./Dockerfile"
+PUSHIT=
 print_usage() {
   printf "\n\nUsage:------------------------------\n"
   printf "Usage: %s -t yourtag\n" "${0}"
-  printf "       If -t (tag)        flag is not specified it will use '%s'\n" $TAG
+  printf "       If -t <tag>   flag is not specified it will use '%s'\n" $TAG
+  printf "       If -p         flag is not specified it will not push '%s'\n" $PUSHIT
   # printf "       If -d (Dockerfile) flag is not specified it will use '%s'\n\n\n" $DOCKERFILE
 }
 
-while getopts 't:s:d:' flag; do
+while getopts 'pt:s:d:' flag; do
   case "${flag}" in
     t) TAG="${OPTARG}" ;;
     # d) DOCKERFILE="${OPTARG}" ;;
+    p) PUSHIT=true ;;
     *) print_usage
        exit 1 ;;
   esac
@@ -28,7 +31,13 @@ done
 
 
 echo "Your image will be build with this repository/tag: '${GITHUB_REPOSITORY}:${TAG}'"
+if [[ $PUSHIT == true ]]
+then
+  echo "and it will be pushed to the docker registry"
+else
+  echo "it will NOT be pushed to the docker registry"
 
+fi
 read -p "Are you sure?(y/n) " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -39,4 +48,8 @@ then
 fi
 
 docker build --tag "${GITHUB_REPOSITORY}:${TAG}" -f "${DOCKERFILE}" .
-docker push "${GITHUB_REPOSITORY}:${TAG}"
+if [[ $PUSHIT == true ]]
+then
+  echo "push it"
+  # docker push "${GITHUB_REPOSITORY}:${TAG}"
+fi
