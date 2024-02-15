42ef4274b54d4e034ab2ff2cd19d08404d5085c4
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-02-15 16:29:07 +0100

    chore: refactoring

--- harvester/build_radolan_grid.py
+++ harvester/build_radolan_grid.py
@@ -7,14 +7,6 @@ def build_radolan_grid(db_conn):
 
     limit_days = 30
 
-    # start date is 'limit_days' days before now, starting at 00h:50min:00s
-    start_date = datetime.now() + timedelta(days=-limit_days)
-    start_date = start_date.replace(hour=0, minute=50, second=0, microsecond=0)
-
-    # end date is yesterday at 23h:50min:00s
-    end_date = datetime.now() + timedelta(days=-1)
-    end_date = end_date.replace(hour=23, minute=50, second=0, microsecond=0)
-
     grid = []
     with db_conn.cursor() as cur:
         cur.execute(
@@ -39,26 +31,31 @@ def build_radolan_grid(db_conn):
         grid = cur.fetchall()
         db_conn.commit()
 
-    # build clean, sorted arrays
+    end_date = datetime.now() + timedelta(days=-1)
+    end_date = end_date.replace(hour=23, minute=50, second=0, microsecond=0)
+    start_date = datetime.now() + timedelta(days=-limit_days)
+    start_date = start_date.replace(hour=0, minute=50, second=0, microsecond=0)
+
     grid_radolan_values = []
     for cell in grid:
+        # cell structure:
+        # index 0 => geometry id
+        # index 1 => full geojson
+        # index 2 => list of datetime
+        # index 3 => list of radolan values
         measured_dates = cell[2]
         measured_radolan_values = cell[3]
-
-        end_date = datetime.now() + timedelta(days=-1)
-        end_date = end_date.replace(hour=23, minute=50, second=0, microsecond=0)
-        start_date = datetime.now() + timedelta(days=-limit_days)
-        start_date = start_date.replace(hour=0, minute=50, second=0, microsecond=0)
         radolan_values_for_cell = []
-        while start_date <= end_date:
+        loop_date = start_date
+        while loop_date <= end_date:
             found = False
             for date_index, date in enumerate(measured_dates):
-                if start_date == date:
+                if loop_date == date:
                     found = True
                     radolan_values_for_cell.append(measured_radolan_values[date_index])
             if found == False:
                 radolan_values_for_cell.append(0)
-            start_date += timedelta(hours=1)
+            loop_date += timedelta(hours=1)
         grid_radolan_values.append(radolan_values_for_cell)
 
     formatted_grid_radolan_values = []
