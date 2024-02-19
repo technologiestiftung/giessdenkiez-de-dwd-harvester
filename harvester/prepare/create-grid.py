d3f8370db7eb8e705bedfd39dfd70ef692e61c2d
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-02-19 11:25:04 +0100

    chore: delete unused files

--- harvester/prepare/create-grid.py
+++ harvester/prepare/create-grid.py
@@ -16,6 +16,7 @@ import numpy
 import linecache
 from shapely.wkt import dumps
 from dotenv import load_dotenv
+
 load_dotenv()
 
 logging.basicConfig()
@@ -37,8 +38,7 @@ temp = "temp"
 # check if all required environmental variables are accessible
 for env_var in ["PG_DB", "PG_PORT", "PG_USER", "PG_PASS", "PG_SERVER"]:
     if env_var not in os.environ:
-        logging.error(
-            "❌Environmental Variable {} does not exist".format(env_var))
+        logging.error("❌Environmental Variable {} does not exist".format(env_var))
 
 pg_server = os.getenv("PG_SERVER")
 pg_port = os.getenv("PG_PORT")
@@ -58,7 +58,7 @@ except:
     conn = None
 
 # we need to give each grid cell a unique value, otherwise gdal_polygonize will combine cells with equal values
-base_grid_file = "grid-germany.asc"
+base_grid_file = "../assets/grid-germany.asc"
 asc_data = numpy.loadtxt(base_grid_file, skiprows=6)
 col_value = 1
 for r_idx, row in enumerate(asc_data):
@@ -66,32 +66,53 @@ for r_idx, row in enumerate(asc_data):
         asc_data[r_idx][c_idx] = col_value
         col_value += 1
 
-header = linecache.getline(base_grid_file, 1) + \
-    linecache.getline(base_grid_file, 2) + \
-    linecache.getline(base_grid_file, 3) + \
-    linecache.getline(base_grid_file, 4) + \
-    linecache.getline(base_grid_file, 5) + \
-    linecache.getline(base_grid_file, 6)
+header = (
+    linecache.getline(base_grid_file, 1)
+    + linecache.getline(base_grid_file, 2)
+    + linecache.getline(base_grid_file, 3)
+    + linecache.getline(base_grid_file, 4)
+    + linecache.getline(base_grid_file, 5)
+    + linecache.getline(base_grid_file, 6)
+)
 
-numpy.savetxt(temp + "/grid-transform.asc", asc_data,
-              header=header.rstrip(), comments='', fmt='%i')
+numpy.savetxt(
+    temp + "/grid-transform.asc",
+    asc_data,
+    header=header.rstrip(),
+    comments="",
+    fmt="%i",
+)
 
 # use gdalwarp as commandline tool to crop the example weather dataset to the outline of our area of interest that is defined by the file buffer.shp
 # with the arguments -s_srs and -t-srs we set the spatial reference systems for the source and the target files
 # for our example weather data the srs is: "+proj=stere +lon_0=10.0 +lat_0=90.0 +lat_ts=60.0 +a=6370040 +b=6370040 +units=m"
-cmdline = ['gdalwarp', temp + "/grid-transform.asc", temp + "/grid-buffer.asc", "-s_srs", "+proj=stere +lon_0=10.0 +lat_0=90.0 +lat_ts=60.0 +a=6370040 +b=6370040 +units=m",
-           "-t_srs", "+proj=stere +lon_0=10.0 +lat_0=90.0 +lat_ts=60.0 +a=6370040 +b=6370040 +units=m", "-r", "near", "-of", "GTiff", "-cutline", "buffer.shp"]
+cmdline = [
+    "gdalwarp",
+    temp + "/grid-transform.asc",
+    temp + "/grid-buffer.asc",
+    "-s_srs",
+    "+proj=stere +lon_0=10.0 +lat_0=90.0 +lat_ts=60.0 +a=6370040 +b=6370040 +units=m",
+    "-t_srs",
+    "+proj=stere +lon_0=10.0 +lat_0=90.0 +lat_ts=60.0 +a=6370040 +b=6370040 +units=m",
+    "-r",
+    "near",
+    "-of",
+    "GTiff",
+    "-cutline",
+    "../assets/buffer.shp",
+]
 subprocess.call(cmdline)
 
-# use gdal_polygonize to transform the grid into a vector. a polygon is created for each pixel in our area of interest with a unique ID (MYFLD). these polygons form our grid and are saved in a shapefile. 
+# use gdal_polygonize to transform the grid into a vector. a polygon is created for each pixel in our area of interest with a unique ID (MYFLD). these polygons form our grid and are saved in a shapefile.
 cmdline = [
     "gdal_polygonize.py",
     temp + "/grid-buffer.asc",
-    "-f", "ESRI Shapefile",
+    "-f",
+    "ESRI Shapefile",
     temp + "/grid.shp",
     "temp",
     "MYFLD",
-    "-q"
+    "-q",
 ]
 
 subprocess.call(cmdline)
@@ -99,8 +120,8 @@ subprocess.call(cmdline)
 df = geopandas.read_file(temp + "/grid.shp")
 df = df.to_crs("epsg:4326")
 
-if df['geometry'].count() > 0:
-    clean = df[(df['MYFLD'].notnull())]  # (df['MYFLD'] > 0) &
+if df["geometry"].count() > 0:
+    clean = df[(df["MYFLD"].notnull())]  # (df['MYFLD'] > 0) &
     if len(clean) > 0:
         values = []
         for index, row in clean.iterrows():
@@ -111,12 +132,13 @@ if df['geometry'].count() > 0:
             psycopg2.extras.execute_batch(
                 cur,
                 "INSERT INTO public.radolan_geometry (geometry) VALUES (ST_GeomFromText(%s, 4326));",
-                values
+                values,
             )
             conn.commit()
 
             cur.execute(
-                "UPDATE public.radolan_geometry SET centroid = ST_Centroid(geometry);")
+                "UPDATE public.radolan_geometry SET centroid = ST_Centroid(geometry);"
+            )
             conn.commit()
 
             cur.close()
