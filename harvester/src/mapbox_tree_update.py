95f719629398ada03377498e06695c878799e69c
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-04-25 14:28:45 +0200

    feat: refactoring & cleanup (#116)
    * feat: start refactoring
    
    * feat: first working draft
    
    * chore: refactoring
    
    * chore: cleanup
    
    * chore: documentation
    
    * chore: more docs
    
    * feat: refactoring
    
    * chore: file structuring
    
    * chore: update readme
    
    * chore: cleanup
    
    * fix: spelling, update Dockerfile
    
    * chore: delete unused files
    
    * fix: bring back shx file
    
    * fix: bring back files
    
    * fix: keep only files needed
    
    * chore: readme update
    
    * fix: missing files
    
    * fix: environment vars, function calls
    
    * fix: add LIMI_DAYS env to test
    
    * fix: add missing env
    
    * fix: typo
    
    * fix: pass SURROUNDING_SHAPE_FILE via env
    
    * fix: create buffer shapefile first
    
    * fix: Dockerfile run command
    
    * fix: path to buffer file
    
    * chore: some logging
    
    * fix: remove -s_srs because it can be inferred from data
    
    * chore: deps update
    
    * fix: revert to last working version
    
    * feat: docs for env, cleanup
    
    * fix: Dockerfile
    
    * fix: Dockerfile
    
    * feat: add watering_sum to Mapbox layer
    
    * fix: string interpolation
    
    * feat: add total water sum to mapbox layer
    
    * chore: unit explanation
    
    * fix: accumulate all waterings of last 30 days, but not the ones of today
    
    * fix: use f strings

---
+++ harvester/src/mapbox_tree_update.py
@@ -0,0 +1,156 @@
+import os
+import tempfile
+import subprocess
+from datetime import datetime
+import logging
+from tqdm import tqdm
+from mapbox_utils import (
+    upload_to_mapbox_storage,
+    start_tileset_creation,
+    wait_for_tileset_creation_complete,
+)
+from supabase_utils import upload_file_to_supabase_storage
+
+
+def preprocess_trees_csv(trees_csv_full_path, temp_dir):
+    """Preprocesses the given trees.csv file with tippecanoe to fulfill the requirements from Mapbox
+       to create a tileset
+
+    Args:
+        trees_csv_full_path (str): the full path to the trees.csv
+        temp_dir (str): the full path to the directory to store the preprocessed file
+
+    Returns:
+        str: full path to the preprocessed trees file
+    """
+    logging.info("Preprocessing trees.csv with tippecanoe...")
+    trees_preprocessed_full_path = os.path.join(temp_dir, "trees-preprocessed.mbtiles")
+    subprocess.call(
+        [
+            "tippecanoe",
+            "-zg",
+            "-o",
+            trees_preprocessed_full_path,
+            "--force",
+            "--drop-fraction-as-needed",
+            trees_csv_full_path,
+        ]
+    )
+    return trees_preprocessed_full_path
+
+
+def generate_trees_csv(temp_dir, db_conn):
+    """Generate a trees.csv file containing all trees currently in the databae
+
+    Args:
+        temp_dir (str): the full path to the directory to store the preprocessed file
+        db_conn: the database connection
+
+    Returns:
+        str: full path to the trees.csv file
+    """
+    logging.info(f"Generating trees.csv...")
+    current_year = datetime.now().year
+    with db_conn.cursor() as cur:
+
+        # Fetch all trees from database
+        cur.execute(
+            # WARNING: The coordinates in the database columns lat and lng are mislabeled! They mean the opposite.
+            """
+            SELECT
+                trees.id,
+                trees.lat,
+                trees.lng,
+                trees.radolan_sum,
+                trees.pflanzjahr,
+                (SELECT COALESCE(SUM(w.amount), 0)::INT AS total_amount FROM trees_watered w WHERE w.timestamp >= CURRENT_DATE - INTERVAL '30 days' AND DATE_TRUNC('day', w.timestamp) < CURRENT_DATE AND w.tree_id = trees.id) as watering_sum
+            FROM
+                trees
+            WHERE
+                ST_CONTAINS(ST_SetSRID ((
+                        SELECT
+                            ST_EXTENT (geometry)
+                            FROM radolan_geometry), 4326), trees.geom)
+            """
+        )
+        trees = cur.fetchall()
+        logging.info(f"Creating trees.csv file for {len(trees)} trees...")
+
+        # Build CSV file with all trees in it
+        header = "id,lng,lat,radolan_sum,age,watering_sum,total_water_sum_liters"
+        lines = []
+        for tree in tqdm(trees):
+            id = tree[0]
+            lat = tree[1]
+            lng = tree[2]
+            # precipitation height in 0.1 mm per square meter
+            # 1mm on a square meter is 1 liter
+            # e.g. value of 380 = 0.1 * 380 = 38.0 mm * 1 liter = 38 liters
+            radolan_sum = tree[3]
+            pflanzjahr = tree[4]
+            watering_sum = tree[5]
+            age = int(current_year) - int(pflanzjahr) if int(pflanzjahr) != 0 else ""
+            # calculated in liters to be easily usable in the frontend
+            total_water_sum_liters = (radolan_sum / 10) + watering_sum
+            line = f"{id}, {lat}, {lng}, {radolan_sum}, {age}, {watering_sum}, {total_water_sum_liters}"
+            lines.append(line)
+        trees_csv = "\n".join([header] + lines)
+        trees_csv_full_path = os.path.join(temp_dir, "trees.csv")
+        with open(trees_csv_full_path, "w") as out:
+            out.write(trees_csv)
+
+        return trees_csv_full_path
+
+
+def update_mapbox_tree_layer(
+    mapbox_username,
+    mapbox_token,
+    mapbox_tileset,
+    mapbox_layer_name,
+    supabase_url,
+    supabase_bucket_name,
+    supabase_service_role_key,
+    db_conn,
+):
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # Generate trees.csv from trees in database
+        trees_csv_full_path = generate_trees_csv(temp_dir, db_conn)
+
+        # Preprocess trees.csv with tippecanoe
+        trees_preprocessed_full_path = preprocess_trees_csv(
+            trees_csv_full_path, temp_dir
+        )
+
+        # Upload preprocessed trees to Supabase storage
+        upload_file_to_supabase_storage(
+            supabase_url,
+            supabase_bucket_name,
+            supabase_service_role_key,
+            trees_preprocessed_full_path,
+            "trees-preprocessed.mbtiles",
+        )
+
+        # Start the Mapbox tileset creating
+        mapbox_storage_credentials = upload_to_mapbox_storage(
+            trees_preprocessed_full_path,
+            mapbox_username,
+            mapbox_token,
+        )
+
+        tileset_generation_id = start_tileset_creation(
+            mapbox_storage_credentials,
+            mapbox_username,
+            mapbox_tileset,
+            mapbox_layer_name,
+            mapbox_token,
+        )
+
+        tileset_creation_error = wait_for_tileset_creation_complete(
+            tileset_generation_id,
+            mapbox_username,
+            mapbox_token,
+        )
+
+        if tileset_creation_error is not None:
+            logging.error("Could not create Mapbox tileset")
