e6d0b7545ebcfdc3361465d41f1b89506a900648
ff6347 <hash@ff6347.email>
2024-04-26 14:27:35 +0200

    chore(housekeeping): Remove terraform files
    Not using tf anymore, db and storage are on supabase

--- terraform/ecs-harvester/data.tf
+++
@@ -1,9 +0,0 @@
-# data.tf
-
-# Fetch AZs in the current region
-data "aws_availability_zones" "available" {
-}
-
-# data "aws_vpc" "default" {
-#   id = "${var.vpc_id}"
-# }
--- terraform/ecs-harvester/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  profile = var.profile
-  region  = var.region
-  version = "~> 3.0"
-}
-
-provider "random" {
-  version = "~> 2.2"
-}
