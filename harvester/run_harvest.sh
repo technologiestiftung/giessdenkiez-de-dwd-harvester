728e4b618aeb6611453271c19f148cc4a76aa62e
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-06-20 14:21:15 +0200

    fix: run harvester independent of previous fail

--- harvester/run_harvest.sh
+++ harvester/run_harvest.sh
@@ -2,11 +2,15 @@
 set -e
 
 python ./src/run_daily_weather.py || {
-    echo "run_daily_weather.py failed";
-    exit 1;
+    echo "run_daily_weather.py failed"
+    FAILED=1
 }
 
 python ./src/run_harvester.py || {
-    echo "run_harvester.py failed";
-    exit 1;
+    echo "run_harvester.py failed"
+    FAILED=1
 }
+
+if [ "$FAILED" = "1" ]; then
+    exit 1
+fi
